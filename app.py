5613ff373bb49e5ca61f751d59fd9805437a16a1
Cody Morris <codyc.morris@gmail.com>
2021-09-25 11:15:13 -0400

    Final edits

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import pymongo
-from bs4 import BeautifulSoup as bs
-import os
-import requests
-import pandas as pd
-from webdriver_manager.chrome import ChromeDriverManager
-from splinter import Browser
+# import pymongo
+# from bs4 import BeautifulSoup as bs
+# import os
+# import requests
+# import pandas as pd
+# from webdriver_manager.chrome import ChromeDriverManager
+# from splinter import Browser
 import scrape_mars
 
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/"
-mongo = PyMongo(app)
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/"
+# mongo = PyMongo(app)
 
-conn = 'mongodb://localhost:27017'
-client = pymongo.MongoClient(conn)
+# conn = 'mongodb://localhost:27017'
+# client = pymongo.MongoClient(conn)
 
-db = client.Mars_db
+# db = client.Mars_db
 
 # listings = db.items.find()
 
@@ -26,27 +26,25 @@ db = client.Mars_db
 #     print(listing)
 
 # Or set inline
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/phone_app")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 
 @app.route("/")
 def index():
-    listings = db.items.find_one()
+    listings = mongo.db.collection.find_one()
     # print(listings)
     return render_template("index.html", listings=listings)
 
 @app.route("/scrape")
 def scraper():
     # listings = mongo.db.listings
-    listings = db.items.find_one()
-    title = scrape_mars.cleantitles
+    # listings = mongo.db.collection.find_one()
+    title = scrape_mars.scrape()
     print(title)
-    listings.update({}, title, upsert=True)
-    return render_template("Clean.html", title=title, listings=listings)
+    mongo.db.collection.update({}, title, upsert=True)
+    return redirect("/")
 
     
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -1,112 +1,128 @@
+import re
 from bs4 import BeautifulSoup as bs
 import os
 import requests
 import pymongo
+import time
 import pandas as pd
 from webdriver_manager.chrome import ChromeDriverManager
 from splinter import Browser
 
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
-browser.visit('https://redplanetscience.com/')
-html = browser.html
+
 
 def scrape():
+    # data = {}
+    executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
-    title, parapraph = news(browser)
-    data = {
-        'news_title': title,
-        'paragraph': parapraph
-    }
-
-
-
-    return data
-
-
-def news(browser):
     browser.visit('https://redplanetscience.com/')
-    title = browser.find_by_css('div.content_title').text
-    paragraph = browser.find_by_css('div.article_teaser_body').text
-    return title, paragraph
-
-
-
-soup = bs(html, 'html.parser')
-# Retrieve all elements that contain book information
-titles = soup.find_all('div', class_='content_title')
-
-cleantitles = []
-
-for title in titles:
-    # print (title.text)
-    cleantitles.append(title.text)
-
-print(cleantitles) 
-
-
-# %%
-paras = soup.find_all('div', class_='article_teaser_body')
-
-cleanparas = []
-
-for para in paras:
-    # print (title.text)
-    cleanparas.append(para.text)
-
-print(cleanparas) 
-
-
-# %%
-browser.visit('https://spaceimages-mars.com/')
-html = browser.html
-
-soup = bs(html, 'html.parser')
-    # Retrieve all elements that contain book information
-pics = soup.find_all('img', class_='thumbimg')
-
-cleanpics = []
-
-for pic in pics:
-    # print (title.text)
-    cleanpics.append(pic['src'])
-
-print(cleanpics) 
-
-
-# %%
-df = pd.read_html('https://galaxyfacts-mars.com/')[0].to_html() 
-# df[1]
-
-
-# %%
-cleanoriginaltitles = []
-cleanoriginalpics = []
-
-for counter in range(4):
-    browser.visit('https://marshemispheres.com/')
-    browser.find_by_css('a.product-item img')[counter].click()
+    # time.sleep(2)
     html = browser.html
+    # browser = Browser('chrome', **executable_path, headless=False)
+    # title, parapraph = news(browser)
+    
+
+
+
+    # return data
+
+
+# def news(browser):
+    # browser.visit('https://redplanetscience.com/')
+    # title = browser.find_by_css('div.content_title').text
+    # paragraph = browser.find_by_css('div.article_teaser_body').text
+    # return title, paragraph
+
+
+
     soup = bs(html, 'html.parser')
+    # time.sleep(3)
     # Retrieve all elements that contain book information
-    originalpics = soup.find_all('a', text='Original')
-    originaltitles = soup.find_all('h2', class_='title')
-    
-    for originalpic in originalpics:
-        cleanoriginalpics.append(originalpic['href'])
+    titles = soup.find('div', class_="content_title")
+    print(f'************************************************************************************************{titles}')
 
-    for originaltitle in originaltitles:
-        cleanoriginaltitles.append(originaltitle.text)
-    
-print(cleanoriginalpics)
-print(cleanoriginaltitles) 
+    cleantitles = []
+
+    # for title in titles:
+        # print (title.text)
+    cleantitles.append(titles.text)
+
+    print(cleantitles) 
 
 
-# %%
-hemisphere_image_urls = []
+    # %%
+    paras = soup.find('div', class_='article_teaser_body')
 
-for counter in range(4):
-    hemisphere_image_urls.append({'Titles': cleanoriginaltitles[counter],'img_url': cleanoriginalpics[counter]})
+    cleanparas = []
 
-print(hemisphere_image_urls)
+    # for para in paras:
+        # print (title.text)
+    cleanparas.append(paras.text)
 
+    print(cleanparas) 
+
+
+    # %%
+    browser.visit('https://spaceimages-mars.com/')
+    # time.sleep(2)
+    html = browser.html
+
+    soup = bs(html, 'html.parser')
+    # time.sleep(3)
+        # Retrieve all elements that contain book information
+    pics = soup.find('img', class_='thumbimg')
+
+    cleanpics = []
+
+    # for pic in pics:
+        # print (title.text)
+    cleanpics.append(pics['src'])
+
+    print(cleanpics) 
+
+
+    # %%
+    df = pd.read_html('https://galaxyfacts-mars.com/')[0].to_html() 
+    # df[1]
+    df= df.replace('\n','')
+
+
+    # %%
+    cleanoriginaltitles = []
+    cleanoriginalpics = []
+
+    for counter in range(4):
+        browser.visit('https://marshemispheres.com/')
+        browser.find_by_css('a.product-item img')[counter].click()
+        # time.sleep(2)
+        html = browser.html
+        soup = bs(html, 'html.parser')
+        # time.sleep(3)
+        # Retrieve all elements that contain book information
+        originalpics = soup.find('a', text='Original')
+        originaltitles = soup.find('h2', class_='title')
+        
+        # for originalpic in originalpics:
+        cleanoriginalpics.append(originalpics['href'])
+
+        # for originaltitle in originaltitles:
+        cleanoriginaltitles.append(originaltitles.text)
+        
+    print(cleanoriginalpics)
+    print(cleanoriginaltitles) 
+
+
+    # %%
+    hemisphere_image_urls = []
+
+    for counter in range(4):
+        hemisphere_image_urls.append({'Titles': cleanoriginaltitles[counter],'img_url': cleanoriginalpics[counter]})
+
+    print(hemisphere_image_urls)
+
+    data = {
+        'Titles': cleanoriginaltitles,
+        'paragraph': cleanparas,
+        'img_url' : cleanoriginalpics,
+        'table' : df
+    }
+    return (data)
